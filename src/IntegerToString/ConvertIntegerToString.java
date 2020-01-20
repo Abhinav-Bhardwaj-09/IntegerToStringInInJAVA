221d8e2df4b07ddf4948a9d5396130c7341515b5
ABhinavRajput-theCreator <abhinav.bhardwaj_cs18@gla.ac.in>
2020-01-20 23:37:23 +0530

    Initial commit

---
+++ src/IntegerToString/ConvertIntegerToString.java
@@ -0,0 +1,13 @@
+package IntegerToString;
+
+import java.util.*;
+
+public class ConvertIntegerToString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter only integer ( Don't enter Character or String ) :    ");
+        int n = sc.nextInt();
+        String s = String.valueOf(n);
+
+        }
+    }
